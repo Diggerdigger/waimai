c9050e3aa33514caaada4dbecfa1dd4edd6e8af7
sheng  <shengjunwu123@163.com>
2020-01-08 16:17:36 +0800

    a

---
+++ src/test/java/com/horacewu/waimai/WaimaiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.horacewu.waimai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class WaimaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
