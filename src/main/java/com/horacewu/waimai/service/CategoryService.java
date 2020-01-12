954ed360374d9d24631712dd3ac09f3302a62f43
sheng <shengjunwu123@163.com>
2020-01-12 20:57:54 +0800

    a

---
+++ src/main/java/com/horacewu/waimai/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.horacewu.waimai.service;
+
+import com.horacewu.waimai.dataobject.ProductCategory;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    ProductCategory findOne(Integer categoryId);
+
+    List<ProductCategory> findAll();
+
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+
+    ProductCategory save(ProductCategory productCategory);
+
+}
